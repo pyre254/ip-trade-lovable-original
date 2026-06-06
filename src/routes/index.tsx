c6017f0d3d740af207f08810f2b2bb81d073a8be
pyre254 <dropsair74@gmail.com>
2026-06-07 00:30:44 +0300

    check tanstack config

---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>IP Trade Marketplace</h1>
+      <p>TanStack Start SSR is working 🚀</p>
+    </div>
+  );
+}
